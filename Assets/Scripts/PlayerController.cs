512a6d2d66172605096d4fe71065e13a520959fa
JamesThePaladin <Jamesthepaladin@gmail.com>
2020-10-06 21:23:52 -0700

    Transfer Commit
    Transfered project to a git repository.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Controller
+{
+    //store movement data
+    public Vector2 movement; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pawn = GetComponent<Pawn>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set movement x and y values to appropriate axis
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        //exit application build
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        pawn.Move(movement);
+    }
+}
---
+++ Assets/Scripts/Repairable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repairable : MonoBehaviour
+{
+    public GameObject fixedBot;
+    float tarnish = 100;
+    float repairRate = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Repair() 
+    {
+       tarnish = tarnish - repairRate * Time.deltaTime;
+    }
+
+    public void Repaired() 
+    {
+        if (tarnish < 1) 
+        {
+            Instantiate(fixedBot, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+}
