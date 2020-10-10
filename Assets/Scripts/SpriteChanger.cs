dc28dff767c6498bcb0116958ed6184e509028b8
JamesThePaladin <Jamesthepaladin@gmail.com>
2020-10-09 22:12:47 -0700

    Decals & Robots Update
    Added sprites/animations for damaged/fixed robots and added decals to the level.

--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -8,13 +8,18 @@ public class SpriteChanger : MonoBehaviour
     // Declare our variables
     private SpriteRenderer sr; // variable for our renderer
     public Color spriteColor; // variable for our Color
-
     // Use this for initialization
     void Start()
     {
+        float randR = Random.Range(0f, 1f);
+        float randG = Random.Range(0f, 1f);
+        float randB = Random.Range(0f, 1f);
         // Load the SpriteRenderer component from the same object this component is on.
         sr = gameObject.GetComponent<SpriteRenderer>();
         // Change the color from our color picker so that the alpha is 1
+        spriteColor.r = randR;
+        spriteColor.g = randG;
+        spriteColor.b = randB;
         spriteColor.a = 1.0f;
         // As long as theRenderer has been set
         if (sr != null)
